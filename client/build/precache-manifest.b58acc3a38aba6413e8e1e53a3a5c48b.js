d3800b10fe38030d2f774d461c59acaf6ada857d
Korman Chen <korchen@ucdavis.edu>
2020-06-05 20:43:28 -0700

    client production build

---
+++ client/build/precache-manifest.b58acc3a38aba6413e8e1e53a3a5c48b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ffcb9761ee0df00dbc50b97f741bfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45a04359fbbb58de3751",
+    "url": "/static/css/main.bed1851b.chunk.css"
+  },
+  {
+    "revision": "b3f8b5b8f2c62ad39ccb",
+    "url": "/static/js/2.ec28c358.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.ec28c358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45a04359fbbb58de3751",
+    "url": "/static/js/main.b3387e41.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "184843881820ca3e3422f3a1c43a4f04",
+    "url": "/static/media/empty-star.18484388.svg"
+  },
+  {
+    "revision": "ba26e0340086b4d739928ad17ab96cd8",
+    "url": "/static/media/food_fight.ba26e034.jpg"
+  },
+  {
+    "revision": "f10a25eb07f2740619c9d181f900f2c6",
+    "url": "/static/media/full-star.f10a25eb.svg"
+  },
+  {
+    "revision": "899194f34fe522e7a78fed3c2c2f262f",
+    "url": "/static/media/half-star.899194f3.svg"
+  },
+  {
+    "revision": "9524f560fa51b1de9faff73fdd6dbe1c",
+    "url": "/static/media/heart.9524f560.svg"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ce05f3e4aba531c548f99d0f1adcaefe.js"
+  "/precache-manifest.b58acc3a38aba6413e8e1e53a3a5c48b.js"
 );
 
 self.addEventListener('message', (event) => {
